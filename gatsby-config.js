4754b5dc82cb4088318ed01ff52202447a238ca8
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-06-20 15:43:09 +0800

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: 'My First Gatsby Site',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass'
+  ],
+}
---
+++ src/pages/page-3.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+export default () => (
+  <div>
+    <h1>Hi from the third page</h1>
+    <p>Welcome to page 3</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
