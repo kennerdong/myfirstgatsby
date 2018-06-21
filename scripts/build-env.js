65688dba7146fbfd386b3d715f33cd2e5c4532c9
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-06-22 04:15:29 +0800

    updated build script to write multi lines

--- scripts/build-env.js
+++ scripts/build-env.js
@@ -1,9 +1,13 @@
 require("dotenv").config({
     path: `.env.${process.env.NODE_ENV}`,
 });
+
+const content = 
+`TEST_KEY=${process.env.TEST_KEY}\n`+
+`MAILCHIMP_API_ENDPOINT=${process.env.MAILCHIMP_API_ENDPOINT}\r\n`;
+
 const fs = require('fs');
 fs.writeFileSync(
     './.env.production', 
-    `TEST_KEY=${process.env.TEST_KEY}\n`
-    `MAILCHIMP_API_ENDPOINT=${process.env.MAILCHIMP_API_ENDPOINT}\n`
+    content
 );
