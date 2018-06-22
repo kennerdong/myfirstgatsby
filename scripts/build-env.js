cfea7ff6b88caa38dfee82eb86a87d3a552a8da0
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-06-23 03:54:24 +0800

    add console log to print environment file contents

--- scripts/build-env.js
+++ scripts/build-env.js
@@ -4,7 +4,8 @@ require("dotenv").config({
 
 const content = 
 `TEST_KEY=${process.env.TEST_KEY}\n`+
-`MAILCHIMP_API_ENDPOINT=${process.env.MAILCHIMP_API_ENDPOINT}\r\n`;
+`MAILCHIMP_API_ENDPOINT=${process.env.MAILCHIMP_API_ENDPOINT}\r\n`+
+`MAILCHIMP_API_KEY=${process.env.MAILCHIMP.API_KEY}`;
 
 const fs = require('fs');
 fs.writeFileSync(
